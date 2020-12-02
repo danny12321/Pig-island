c8c66607553137d6f185a746ddae0ac267b02ab1
Danny <d.berkelaar1@student.avans.nl>
2020-12-02 13:45:40 +0100

    Initial commit

---
+++ cmake-build-debug/pigisland/CMakeFiles/pigisland-resources.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pigisland-resources"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pigisland-resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
