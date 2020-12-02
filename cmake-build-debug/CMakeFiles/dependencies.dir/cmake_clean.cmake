b030c3a84fbc8dc12d3b611843a6ed8297896280
Danny <d.berkelaar1@student.avans.nl>
2020-12-02 14:12:32 +0100

    Removed build files, because schools gitignore is not good

--- cmake-build-debug/CMakeFiles/dependencies.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dependencies"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dependencies.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/pigisland/CMakeFiles/pigisland-resources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pigisland-resources"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pigisland-resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
