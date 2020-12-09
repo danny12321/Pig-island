08c450f7f785ff715263164181a59042c048bac8
Thierry Rietveld <thierry.rietveld0505@gmail.com>
2020-12-09 14:45:44 +0100

    mooi

--- pigisland/src/kmint/pigisland/shark.cpp
+++ pigisland/src/kmint/pigisland/shark.cpp
@@ -8,32 +8,44 @@
 #include <kmint/pigisland/state/scared.hpp>
 #include <kmint/pigisland/state/hunt.hpp>
 #include <kmint/pigisland/state/shark_wandering.hpp>
+#include <kmint/pigisland/boat.hpp>
 
 namespace kmint {
-namespace pigisland {
-shark::shark(map::map_graph &g, map::map_node &initial_node) : context(g, initial_node), drawable_{*this,
-                                                     graphics::image {
-                                                         shark_image()}} {
-    setState(new shark_wandering(this));
-}
+    namespace pigisland {
+        shark::shark(map::map_graph &g, map::map_node &initial_node) : context(g, g[69]), drawable_{*this,
+                                                                                                           graphics::image{
+                                                                                                                   shark_image()}} {
+            setState(new shark_wandering(this));
+        }
 
-void shark::act(delta_time dt) {
-  t_passed_ += dt;
-  if (to_seconds(t_passed_) >= 1) {
-    // Do state
-    activeState->execute(dt);
-    stepsMade++;
-    t_passed_ = from_seconds(0);
+        void shark::act(delta_time dt) {
+            t_passed_ += dt;
+            if (to_seconds(t_passed_) >= 1) {
+                // Do state
+                activeState->execute(dt);
+                stepsMade++;
+                t_passed_ = from_seconds(0);
 
-    if(stepsMade == 100) {
-        setState(new tired(this));
-    }
-  }
-}
+                if (stepsMade == 100) {
+                    setState(new tired(this));
+                }
 
-    void shark::setTint(graphics::color color) {
-        drawable_.set_tint(color);
-    }
+                for (auto i = begin_perceived(); i != end_perceived(); ++i) {
+                    auto &a = *i;
 
-} // namespace pigisland
+                    if(typeid(a).name() == typeid(kmint::pigisland::boat).name()) {
+                        if(math::distance(a.location(), this->location()) <= 50) {
+                            setState(new scared(this));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        void shark::setTint(graphics::color color) {
+            drawable_.set_tint(color);
+        }
+
+    } // namespace pigisland
 } // namespace kmint
